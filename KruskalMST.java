ebfd4be91a7e0d5b350be132bf4ddda7d2c8b69f
Aditya Kamble <117978437+Adi7558@users.noreply.github.com>
2024-05-05 11:04:05 +0530

    Update KruskalMST.java

--- KruskalMST.java
+++ KruskalMST.java
@@ -2,17 +2,22 @@ import java.util.*;
 
 public class KruskalMST {
     
+    // Inner class representing an edge in the graph
     class Edge implements Comparable<Edge> {
         int src, dest, weight;
         
+        // Compare method to sort edges based on weight
         public int compareTo(Edge other) {
             return this.weight - other.weight;
         }
     }
     
+    // Method to find the minimum spanning tree using Kruskal's algorithm
     public void kruskal(int[][] graph, int numVertices) {
 
+        // List to store all edges in the graph
         List<Edge> edges = new ArrayList<>();
+        // Generating edges from the adjacency matrix representation of the graph
         for (int i = 0; i < numVertices; i++) {
             for (int j = i + 1; j < numVertices; j++) {
                 if (graph[i][j] != 0) {
@@ -25,54 +30,65 @@ public class KruskalMST {
             }
         }
         
+        // Sorting edges based on their weights
         Collections.sort(edges);
  
+        // Array to keep track of parent nodes in the MST
         int[] parent = new int[numVertices];
         for (int i = 0; i < numVertices; i++) {
-            parent[i] = i;
+            parent[i] = i; // Initializing each vertex as its own parent
         }
 
+        // List to store edges in the MST
         List<Edge> mst = new ArrayList<>();
         
+        // Iterating through sorted edges and adding them to MST if they don't form a cycle
         for (Edge edge : edges) {
-            int srcParent = find(parent, edge.src);
-            int destParent = find(parent, edge.dest);
-            if (srcParent != destParent) {
-                mst.add(edge);
-                parent[srcParent] = destParent;
+            int srcParent = find(parent, edge.src); // Finding parent of source vertex
+            int destParent = find(parent, edge.dest); // Finding parent of destination vertex
+            if (srcParent != destParent) { // If including this edge doesn't create a cycle
+                mst.add(edge); // Add edge to MST
+                parent[srcParent] = destParent; // Update parent of source vertex
             }
         }
-        int sum=0;
+        
+        // Printing the edges in the MST along with their weights
         System.out.println("Edges in the MST:");
+        int sum = 0; // Variable to store the sum of edge weights in MST
         for (Edge edge : mst) {
             System.out.println(edge.src + " - " + edge.dest + " : " + edge.weight);
-            sum+=edge.weight;
+            sum += edge.weight; // Updating sum with edge weight
         }
+        // Printing the minimum weight of the MST
         System.out.println("Minimum weight of MST: " + sum);
     }
     
+    // Method to find the parent of a vertex in the MST
     private int find(int[] parent, int i) {
-        if (parent[i] != i) {
-            parent[i] = find(parent, parent[i]);
+        if (parent[i] != i) { // If the current vertex is not its own parent
+            parent[i] = find(parent, parent[i]); // Recursively find parent until it is found
         }
-        return parent[i];
+        return parent[i]; // Return the parent of the vertex
     }
     
+    // Main method to take input and call Kruskal's algorithm
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        System.out.print("Enter the  size of the graph: ");
-        int n = in.nextInt();
-        int[][] graph = new int[n][n];
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                System.out.print("Enter the weight "+i+ "-> "+j+" of the graph: ");
-                graph[i][j]=in.nextInt();
+        System.out.print("Enter the size of the graph: ");
+        int n = in.nextInt(); // Reading the number of vertices in the graph
+        int[][] graph = new int[n][n]; // Creating adjacency matrix representation of the graph
+        // Taking input for edge weights
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < n; j++) {
+                System.out.print("Enter the weight " + i + " -> " + j + " of the graph: ");
+                graph[i][j] = in.nextInt(); // Reading edge weight from user input
             }
         }
-        KruskalMST kruskal = new KruskalMST();
-        kruskal.kruskal(graph, n);
+        KruskalMST kruskal = new KruskalMST(); // Creating instance of KruskalMST class
+        kruskal.kruskal(graph, n); // Calling Kruskal's algorithm
     }
 }
+
 
 /*
 Enter the  size of the graph: 5
